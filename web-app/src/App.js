524ec773af8312791dede403a8d8435f7b00f9f6
Charlie Ney <59841542+CharlieNey@users.noreply.github.com>
2025-05-06 21:02:47 -0500

    project docs 2?

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -4,19 +4,21 @@ import { BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom';
 import Register from './components/Register';
 import Login from './components/Login';
 import Home from './components/Home';
+import ArtistInfo from './components/ArtistInfo';
 //import ArtistInfo from './components/ArtistInfo'; <ArtistInfo>
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link> | <Link to="/register">Register</Link> | <Link to="/login">Login</Link>
+        <Link to="/">Home</Link> | <Link to="/register">Register</Link> | <Link to="/login">Login</Link> | <Link to="/search-albums">ArtistInfo </Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/search-albums" element={<ArtistInfo />} />
       </Routes>
     </Router>
   );
